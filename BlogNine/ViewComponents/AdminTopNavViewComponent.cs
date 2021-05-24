e8fecdb18409959ced003952a67b8f4eea37849d
Admin <hudaierbas@gmail.com>
2021-05-25 00:05:46 +0300

    admin controller and views

---
+++ BlogNine/ViewComponents/AdminTopNavViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogNine.ViewComponents
+{
+    public class AdminTopNavViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return await Task.Factory.StartNew(() =>
+            {
+                return View();
+            });
+        }
+    }
+}
